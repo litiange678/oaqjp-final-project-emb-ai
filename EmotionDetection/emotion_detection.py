7166b3c0ecf9890d0e80219741916143ff8da712
litiange678 <litiange678@gmail.com>
2026-07-06 08:44:24 -0400

    Initial Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,16 @@
+import requests, json
+
+def emotion_detector(text_to_analyze):
+    '''
+    This function is to generate the emotion detection result
+    '''
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, headers = headers, json = input_json)
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions['dominant_emotion'] = dominant_emotion
+
+    return emotions
